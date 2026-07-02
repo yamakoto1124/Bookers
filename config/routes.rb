2ae5414c529bed194f9378d61558c33e79f68b44
yamakoto1124 <293471220+yamakoto1124@>
2026-07-02 21:08:03 +0000

    [update]Bookersを作成しよう

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get "books/new"
+  get "books/index"
+  get "books/show"
+  get "books/edit"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  post 'books' => 'books#create'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
