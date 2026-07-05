ae94439c65ab6118903c4e953bd6bc0cdf7e6387
yamakoto1124 <293471220+yamakoto1124@>
2026-07-05 10:12:37 +0000

    [update]Bookersを作成しよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,49 @@
-class BooksController < ApplicationController
-  def new
-  end
+ class BooksController < ApplicationController
+   def new
 
-  def index
-  end
+    @book = Book.new
 
-  def show
-  end
+   end
 
-  def edit
-  end
-end
+   def create
+    @book = Book.new(book_params)
+
+   if @book.save
+    redirect_to book_path(@book.id)
+
+   else
+    render :new, status: :unprocessable_entity
+   end
+   end
+
+   def index
+    @books = Book.all
+   end
+
+   def show
+    @book = Book.find(params[:id])
+   end
+
+   def edit
+    @book = Book.find(params[:id])
+   end
+
+   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+
+   end
+
+   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to 'books'
+   end
+
+   private
+
+   def book_params
+    params.require(:book).permit(:title, :body)
+   end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   get "books/new"
-  get "books/index"
-  get "books/show"
-  get "books/edit"
+  
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
